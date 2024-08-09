f982633e31f3ae8fc1b3e194972af0f23c346447
andretosin <andre_tosin@hotmail.com>
2024-08-09 09:49:13 -0300

    add hello world service

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3001;
 
+app.use(cors({
+  origin: 'http://localhost:3000' // Permite apenas essa origem
+}));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+    res.send('Hello from the backend!');
 });
 
 app.listen(port, () => {
-  console.log(`Backend server is running on http://localhost:${port}`);
+    console.log(`Backend server is running on http://localhost:${port}`);
 });
