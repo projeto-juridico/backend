ce78e68a05c325ada4c8ad6fd4921f0e0919a2db
andretosin <andre_tosin@hotmail.com>
2024-08-09 21:51:49 -0300

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors'); 
 
 const app = express();
-const port = 3001;
+const port = 80;
 
 const allowedOrigins = [
     'https://master--amazing-sunburst-566e88.netlify.app',
