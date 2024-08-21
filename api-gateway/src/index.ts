7ef39c0686fa94ceac88456c1095602e220ee448
Andre <andre_tosin@hotmail.com>
2024-08-21 20:40:13 -0300

    add env var

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -7,6 +7,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log(process.env.AUTH_SERVICE)
+
 app.use(express.json());
 app.use("/api", routes);
 
--- api-gateway/src/routes/index.ts
+++ api-gateway/src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { handleRequest } from "../requestHandler";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = Router();
 
 const SERVICE_1_URL = process.env.AUTH_SERVICE as string;
 
-router.all("/auth-service*", (req, res) =>
+router.all("/auth-service*", (req, res) => {
   handleRequest(req, res, SERVICE_1_URL)
-);
+});
 
 export default router;
