89388f059e81e09209824535fbc8c176181c97ea
andretosin <andre_tosin@hotmail.com>
2024-08-21 21:59:21 -0300

    test webhook

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", routes);
