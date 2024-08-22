3a650721f5332378e2803c043aa4b159af3ce6ef
andretosin <andre_tosin@hotmail.com>
2024-08-21 21:04:36 -0300

    add api-gateway dockerfile

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -7,10 +7,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-console.log(process.env.AUTH_SERVICE)
-
 app.use(express.json());
 app.use("/api", routes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("API Gateway is up and running!");
+});
 
 app.listen(PORT, () => {
   console.log(`API Gateway running on http://localhost:${PORT}`);
